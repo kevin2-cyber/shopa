2ff10cc590921c260222c69fa8b48f55c127f6c2
kevin2-cyber <keduful@st.ug.edu.gh>
2024-06-05 00:44:29 +0000

    tried my best

--- src/main/java/com/kimikevin/shopa/ShopaApplication.java
+++ src/main/java/com/kimikevin/shopa/ShopaApplication.java
@@ -1,13 +1,32 @@
 package com.kimikevin.shopa;
 
+import com.kimikevin.shopa.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
 
 @SpringBootApplication
-public class ShopaApplication {
+public class ShopaApplication implements CommandLineRunner {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopaApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		String sql = "SELECT * from PRODUCT";
+
+		List<Product> products = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Product.class));
+
+		products.forEach(System.out::println);
+	}
 }
